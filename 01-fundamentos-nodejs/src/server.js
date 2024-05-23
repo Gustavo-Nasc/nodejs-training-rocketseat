ba98fc02295f5a10318cefc79734aca07af0cee5
Gustavo-Nasc <gustavo.ns@outlook.com.br>
2024-05-23 15:42:07 -0300

    feat: learning how to work with body request data

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -10,28 +10,52 @@ import http from 'node:http'
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
+
+  // Copiando o código da última aula, nós podemos aplicar o conceito e obter o
+  // corpo da requisição que está sendo feita, transformando o Buffer em JSON
+  // Com isso, acessando os dados do objeto individualmente
+  /* */
+  const buffers = []
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+  // const body = JSON.parse(Buffer.concat(buffers).toString())
+  // Ao executar somente com a linha acima, será exibido um erro, pois o código
+  // é exutado até quando não há nenhuma informação no corpo da requisição...
+  // Para consertar, podemos utilizar um 'try/catch'
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    req.body = null
+  }
+
+  // Com isso, podemos desestruturar o body e criar um usuário com os dados
+  // inseridos no corpo de requisição
+  if (method === 'POST' && url === '/usuarios') {
+    if (req.body) {
+      const { name, email } = req.body
+      users.push({
+        id: 1,
+        name, // Equivalente a name: name
+        email, // Equivalente a email: email
+      })
+
+      return res.writeHead(201).end()
+    } else {
+      return res.writeHead(500).end(JSON.stringify({
+        messageError: "Insira os dados do usuário no 'body' da requisição"
+      }))
+    }
+
+  }
+  /* */
 
   if (method === 'GET' && url === '/usuarios') {
       return res
         .setHeader('Content-Type', 'application/json')
         .end(JSON.stringify(users))
-  }
-
-  if (method === 'POST' && url === '/usuarios') {
-    users.push({
-      id: 1,
-      name: 'John Doe',
-      email: 'john.doe@example.com',
-    })
-    // return res.end('Criação de um usuário')
-    // Endentendo agora sobre os Status Codes, podemos retornar ele como status
-    // Ao invés de um texto simples
-
-    return res.writeHead(201).end()
-    // Status 201 é utilizado sempre que é CRIADO
-    // um recurso no Backend
   }
 
   return res.writeHead(404).end('Não encontrado')
