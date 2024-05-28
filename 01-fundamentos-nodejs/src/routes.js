fa653462574b504c58b38254d872388c8e7448e2
Gustavo-Nasc <gustavo.ns@outlook.com.br>
2024-05-28 17:27:02 -0300

    feat: organize application routes

---
+++ 01-fundamentos-nodejs/src/routes.js
@@ -0,0 +1,41 @@
+
+import { randomUUID } from 'node:crypto'
+import { Database } from './database.js'
+
+const database = new Database()
+
+// Para a criação das rotas da nossa aplicação, criamos um Vetor de Rotas, onde
+// cada rota é um objeto, composto do Método, URL e a função que será executada
+export const routes = [
+  {
+    method: 'GET',
+    url: '/users',
+    handler: (req, res) => {
+      const users = database.select('users')
+      return res.end(JSON.stringify(users))
+    }
+  },
+  {
+    method: 'POST',
+    url: '/users',
+    handler: (req, res) => {
+      if (req.body) {
+        const { name, email } = req.body
+  
+        const user = {
+          id: randomUUID(),
+          name,
+          email,
+        }
+  
+        database.insert('users', user)
+  
+        return res.writeHead(201).end()
+      } else {
+        return res.writeHead(500).end(JSON.stringify({
+          messageError: "Insira os dados do usuário no 'body' da requisição"
+        }))
+      }
+    }
+  }
+]
--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,45 +1,22 @@
 import http from 'node:http'
 import { json } from './middlewares/json.js'
-import { Database } from './database.js'
+import { routes } from './routes.js'
 // Utilizaremos o UUID para gerar um ID único para para usuário criado
-// === UUID => Universally Unique IDentifier
-// É um ID único, ou seja, toda vez que um código for gerado, ele será único
-import { randomUUID } from 'node:crypto'
-
-// Com o banco de dados criado, podemos criar uma nova instância do mesmo
-const database = new Database()
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
 
-  if (method === 'POST' && url === '/usuarios') {
-    if (req.body) {
-      const { name, email } = req.body
+  // Como temos acesso ao method e url da requisição, podemos verificar se alguma
+  // rota daquelas de foram criadas é igual a requisição
+  const route = routes.find(route => {
+    return route.method === method && route.url === url
+  })
 
-      const user = {
-        // E no lugar do valor do 'id: 1', subistituímos por 'id: randomUUI()'
-        id: randomUUID(),
-        name,
-        email,
-      }
-
-      database.insert('users', user)
-
-      return res.writeHead(201).end()
-    } else {
-      return res.writeHead(500).end(JSON.stringify({
-        messageError: "Insira os dados do usuário no 'body' da requisição"
-      }))
-    }
-
-  }
-
-  if (method === 'GET' && url === '/usuarios') {
-    const users = database.select('users')
-
-      return res.end(JSON.stringify(users))
+  // Caso a rota exista, executaremos a função 'handler()' dessa rota
+  if (route) {
+    return route.handler(req, res)
   }
 
   return res.writeHead(404).end('Não encontrado')
