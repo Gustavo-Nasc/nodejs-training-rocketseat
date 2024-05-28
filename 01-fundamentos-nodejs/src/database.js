38af97f289a445a4352afa243578c0ff74921e38
Gustavo-Nasc <gustavo.ns@outlook.com.br>
2024-05-27 23:22:04 -0300

    feat: persisting data with 'node:fs'

--- 01-fundamentos-nodejs/src/database.js
+++ 01-fundamentos-nodejs/src/database.js
@@ -1,9 +1,32 @@
+// Para persistir o banco de dados localmente, utilizaremos o 'fs', um módulo do Node
+import fs from 'node:fs'
+
 // Para suprir a necessidade inicial dos dados estarem sendo armazenado em memória
 // podemos criar um banco de dados simples baseado em arquivos físicos
+
+// Podemos criar uma URL padrão onde o arquivo será salvo com a classe global
+// URL, que pode nos retornar a URL atual no dispositivo
+const databasePath = new URL('../db.json', import.meta.url)
 export class Database {
   // Embora nós iniciamos o vetor de 'users' como um vetor, nós iniciamos o DB
   // como um objeto para que possamos salvar mais informações e não apenas um vetor
   #database = {} // A '#' indica que esse é um campo privado da classe Database
+
+  // Construtor que será executado assim que o Database ser carregado
+
+  #persist() {
+    // Utilizamos o 'fs' para persistir os dados num arquivo localmente salvo
+    // E pedimos para o JSON transformar os dados numa estrutura JSON
+    fs
+      .writeFile(databasePath, JSON.stringify(this.#database))
+      .then(data => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => {
+        // Caso o arquivo não exista, ele é criado automaticamente
+        this.#persist()
+      })
+  }
 
   // Função para busca dos dados, onde recebemos o nome da tabela
   select(table) {
@@ -23,6 +46,8 @@ export class Database {
     } else {
       this.#database[table] = [data]
     }
+
+    this.#persist()
 
     return data
   }
