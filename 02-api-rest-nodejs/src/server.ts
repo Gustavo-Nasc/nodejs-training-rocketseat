bca2b24e80188799b2b9dda1aded66fcb7d6d7c0
Gustavo-Nasc <gustavo.ns@outlook.com.br>
2024-05-30 02:57:32 -0300

    feat: learn about 'eslint'

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -6,8 +6,10 @@ app.get('/hello', () => {
   return 'Hello World'
 })
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('HTTP Server running on port 3333')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server running on port 3333')
+  })
