eb1a267a68ecaed45b1cb5ea6b0fea2238a36474
Gustavo-Nasc <gustavo.ns@outlook.com.br>
2024-05-31 14:38:55 -0300

    feat: learning about local variables

--- 02-api-rest-nodejs/src/database.ts
+++ 02-api-rest-nodejs/src/database.ts
@@ -1,9 +1,14 @@
+import 'dotenv/config'
 import { Knex, knex as setupKnex } from 'knex'
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL env is not defined')
+}
 
 export const config = {
   client: 'sqlite',
   connection: {
-    filename: './src/database/app.db',
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
