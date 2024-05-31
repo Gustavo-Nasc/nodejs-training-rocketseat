e06c27bab3be8ab5ad25079cc87d347ed9dfcf64
Gustavo-Nasc <gustavo.ns@outlook.com.br>
2024-05-31 18:29:47 -0300

    feat: add route to create transaction

--- 02-api-rest-nodejs/src/routes/transactions.ts
+++ 02-api-rest-nodejs/src/routes/transactions.ts
@@ -1,10 +1,23 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select('*')
+  app.post('/', async (req, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transactions
+    const { title, amount, type } = createTransactionBodySchema.parse(req.body)
+
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
