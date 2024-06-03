d5a9099192c14f5d910d7e8f7449f0d88a33d529
Gustavo-Nasc <gustavo.ns@outlook.com.br>
2024-06-03 16:28:45 -0300

    test: add tests for summary and specific transaction

--- 02-api-rest-nodejs/src/tests/transactions.spec.ts
+++ 02-api-rest-nodejs/src/tests/transactions.spec.ts
@@ -55,4 +55,65 @@ describe('Transactions routes', () => {
       }),
     ])
   })
+
+  it('should be able to get a specific transaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Teste',
+        amount: 100,
+        type: 'credit',
+      })
+
+    const cookie = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookie!)
+      .expect(200)
+
+    const transactionId = listTransactionsResponse.body.transactions[0].id
+
+    const getTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookie!)
+      .expect(200)
+
+    expect(getTransactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'Teste',
+        amount: 100,
+      }),
+    )
+  })
+
+  it('should be able to get the summary', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Teste',
+        amount: 100,
+        type: 'credit',
+      })
+
+    const cookie = createTransactionResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookie!)
+      .send({
+        title: 'Transação de débito',
+        amount: 50,
+        type: 'debit',
+      })
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookie!)
+      .expect(200)
+
+    expect(summaryResponse.body.summary).toEqual({
+      amount: 50,
+    })
+  })
 })
