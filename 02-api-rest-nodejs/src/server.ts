1dcdd005d9468028e29fd1fd4ec5cbc472c29f31
Gustavo-Nasc <gustavo.ns@outlook.com.br>
2024-06-05 17:21:29 -0300

    chore: add host to 'render'

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,8 +1,10 @@
 import { app } from './app'
 import { env } from './env'
+const host = 'RENDER' in process.env ? '0.0.0.0' : 'localhost'
 
 app
   .listen({
+    host,
     port: env.PORT,
   })
   .then(() => {
