e49d8dbc7313715859e3e94c0e1b840b80104465
Gustavo-Nasc <gustavo.ns@outlook.com.br>
2024-06-27 19:35:45 -0300

    feat: create first migration with prisma and docker in '03-api-solid'

--- 03-api-solid/src/app.ts
+++ 03-api-solid/src/app.ts
@@ -1,3 +1,31 @@
 import fastify from 'fastify'
 
 export const app = fastify()
+
+// Criando um Database com Docker e PostreSQL
+
+// === COMANDO PADRÃO PARA CRIAÇÃO DE CONTAINER DOCKER COM POSTGRESQL
+
+// docker run --name api-solid-pg bitnami/postgresql
+// Após --name passa-se o nome que queremos dar a esse container. E, em seguida,
+// qual a imagem que queremos usar para a criação desse container
+
+// =============================================================================
+// === PRINCIPAIS POSSÍVEIS VARIÁVEIS
+
+// -e POSTGREQL_USERAME=
+// default: postgres
+// O Username que poderá acessar o Container
+
+// -e POSTGREQL_PASSWORD=
+// default: nil
+// A senha para acessar o Container
+
+// -e POSTGRESQL_DATABASE=
+// default: postgres
+// O nome do Database que queremos criar
+
+// -p 5432:5432
+// Indica que queremos direcionar a porta do Container (5432) para a porta do Host
+// que deseja entrar no Container, ou seja, quando formos acessar o Container,
+// podemos ir pelo localhost:5432
