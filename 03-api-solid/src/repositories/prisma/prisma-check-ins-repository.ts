8bbfe3af7fdfc6c13a20dca9761cd04f15076357
Gustavo-Nasc <gustavo.ns@outlook.com.br>
2024-07-10 14:15:42 -0300

    feat: create 'check-ins' and 'users' repositories for prisma

--- 03-api-solid/src/repositories/prisma/prisma-check-ins-repository.ts
+++ 03-api-solid/src/repositories/prisma/prisma-check-ins-repository.ts
@@ -1,8 +1,75 @@
-import { Prisma } from '@prisma/client'
+import { CheckIn, Prisma } from '@prisma/client'
 import { CheckInsRepository } from '../check-ins-repository'
+import { prisma } from '@/lib/prisma'
+import dayjs = require('dayjs')
 
 export class PrismaCheckInsRepository implements CheckInsRepository {
   async create(data: Prisma.CheckInUncheckedCreateInput) {
-    throw new Error('Method not implemented.')
+    const checkIn = await prisma.checkIn.create({
+      data,
+    })
+
+    return checkIn
+  }
+
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const startOfTheDay = dayjs(date).startOf('date')
+    const endOfTheDay = dayjs(date).endOf('date')
+
+    const checkIn = await prisma.checkIn.findFirst({
+      where: {
+        user_id: userId,
+        created_at: {
+          gte: startOfTheDay.toDate(), // gte => greater then or equal
+          lte: endOfTheDay.toDate(), // lower then or equal
+        },
+      }, // Irá procurar um check in do usuário pelo id, onde a data de criação
+      // esteja entre a data inicial e a data final do dia
+    })
+
+    return checkIn
+  }
+
+  async findManyByUserId(userId: string, page: number) {
+    const checkIns = await prisma.checkIn.findMany({
+      where: {
+        user_id: userId,
+      },
+      take: 20, // Quantos itens desejamos trazer, como o 'limit' e 'offset'
+      skip: (page - 1) * 20, // Quantos itens desejamos pular
+    })
+
+    return checkIns
+  }
+
+  async countByUserId(userId: string) {
+    const count = await prisma.checkIn.count({
+      where: {
+        user_id: userId,
+      },
+    })
+
+    return count
+  }
+
+  async findById(id: string) {
+    const checkIn = await prisma.checkIn.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return checkIn
+  }
+
+  async save(data: CheckIn) {
+    const checkIn = await prisma.checkIn.update({
+      where: {
+        id: data.id,
+      },
+      data,
+    })
+
+    return checkIn
   }
 }
